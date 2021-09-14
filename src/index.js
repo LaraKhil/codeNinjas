139e00e2e12564868ff96e675cabcb0f3fcd9fe3
LaraKhil <56624634+LaraKhil@users.noreply.github.com>
2021-09-14 09:56:35 +0300

    added pagination labrary

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss';
+import Pagination from 'tui-pagination';
+
+// Examples for Pagination
+
+// const container = document.getElementById('tui-pagination-container');
+// const instance = new Pagination(container, { ... });
+
+// instance.getCurrentPage();
