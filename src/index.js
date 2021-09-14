76848d344b6cd8751fba671c3f7729ea0b2b11ab
Anatolii Bairov <tolikbairov16@gmail.com>
2021-09-14 16:57:44 +0300

    add modal btn eventListener

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Pagination from 'tui-pagination';
 // instance.getCurrentPage();
 
 import './js/cartset';
-
+import './js/modal-btn';
 // import filmApiService from './js/api-service.js';
 
 // console.log(filmApiService.fetchPopularFilms());
