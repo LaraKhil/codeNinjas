4e866cf441744339b997ecf8590a160202fbb8b3
Evgeniy-the-Great <shalenkoevgeniy@gmail.com>
2021-09-14 17:20:25 +0300

    add check on empty poster

--- src/js/cartset.js
+++ src/js/cartset.js
@@ -1,5 +1,6 @@
 import filmApiService from './api-service';
 import renderPopularFilms from '../Templates/heroCartset.hbs';
+import plug from '../images/plug.png'
 
 const refList = document.querySelector('.hero-list');
 
@@ -9,9 +10,12 @@ filmApiService.fetchAPIGenres()
   .then(({ results }) => {
     const normalData = results.map(movie => {
       const releaseYear = new Date(movie.release_date).getFullYear();
+      const fullPath = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+      const poster = movie.poster_path ? fullPath : plug;
       return {
         ...movie,
         release_date: releaseYear,
+        poster_path: poster,
         genres: filmApiService.genres
           .filter(obj => movie.genre_ids.includes(obj.id))
           .map(genre => genre.name)
