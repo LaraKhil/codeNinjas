ec967e9e071aa4770e831969058a792683f25278
Anatolii Bairov <tolikbairov16@gmail.com>
2021-09-15 19:15:52 +0300

    local storage logic

--- src/js/modalSingleCard.js
+++ src/js/modalSingleCard.js
@@ -1,21 +1,18 @@
 import filmApiService from './api-service';
 import renderModalWindow from '../Templates/modalTemplate.hbs';
-import {refs} from '../js/cartset';
-
+import { refs } from '../js/cartset';
+import modalBtnService from './modal-btn';
 const modalList = document.querySelector('.modal');
 const modalHBS = document.querySelector('.modal__hbs-wrapper');
 
-function onFilmClick(e){
+function onFilmClick(e) {
   const targetId = e.target.id;
-  filmApiService.fetchFilmsById(targetId)
-  .then(data => {
-    
+  filmApiService.fetchFilmsById(targetId).then(data => {
     const renderModal = renderModalWindow(data);
     modalHBS.innerHTML = renderModal;
 
     const modalRefs = {
       modalBtnClose: document.querySelector('.js-modal__btn-close'),
-    
     };
     modalList.classList.remove('show');
 
@@ -24,9 +21,21 @@ function onFilmClick(e){
     }
 
     modalRefs.modalBtnClose.addEventListener('click', onBtnModalClose);
-  });
 
-  
-};
+    /**
+     * обработчики событий для модальных кнопок
+     */
+    modalBtnService.updateBtns(targetId);
+    modalBtnService.refs.queueBtn.addEventListener('click', () =>
+      onAddBtnClick(modalBtnService.localStorageKeys.queueFilm),
+    );
+    modalBtnService.refs.watchBtn.addEventListener('click', () =>
+      onAddBtnClick(modalBtnService.localStorageKeys.watchedFilm),
+    );
+    function onAddBtnClick(key) {
+      modalBtnService.save(key);
+    }
+  });
+}
 
 refs.addEventListener('click', onFilmClick);
